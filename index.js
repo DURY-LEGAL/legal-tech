0609fad624dea6d5674433111e998292a17bf6a1
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:57 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// legal-tech - Legal Tech framework - Tools for legal technology and law firm automation. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
